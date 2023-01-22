25eb25744f8ef3b90259ca7663d8ee83490636e1
WizMe-M <timkin.moxim@mail.ru>
2023-01-22 17:53:58 +0300

    fixed connection string for conduit

--- lib/dart_backend.dart
+++ lib/dart_backend.dart
@@ -24,10 +24,10 @@ class AppService extends ApplicationChannel {
 
   PersistentStore _initDatabase() {
     final username = Platform.environment['DB_USERNAME'] ?? 'postgres';
-    final password = Platform.environment['DB_PASSWORD'] ?? '123';
+    final password = Platform.environment['DB_PASSWORD'] ?? r'password';
     final host = Platform.environment['DB_HOST'] ?? '127.0.0.1';
     final port = int.parse(Platform.environment['DB_PORT'] ?? '5432');
-    final databaseName = Platform.environment['DB_NAME'] ?? 'dart_backend';
+    final databaseName = Platform.environment['DB_NAME'] ?? 'dart-api';
 
     return PostgreSQLPersistentStore(
         username, password, host, port, databaseName);
