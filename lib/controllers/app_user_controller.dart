34eba62b934b59e3893daa8551b1206e9f754dbd
WizMe-M <timkin.moxim@mail.ru>
2023-01-22 20:12:34 +0300

    update profile

--- lib/controllers/app_user_controller.dart
+++ lib/controllers/app_user_controller.dart
@@ -11,14 +11,43 @@ class AppUserController extends ResourceController {
   final ManagedContext managedContext;
 
   @Operation.get()
-  Future<Response> getProfile(@Bind.header(HttpHeaders.authorizationHeader) String header) async {
+  Future<Response> getProfile(
+      @Bind.header(HttpHeaders.authorizationHeader) String header) async {
     try {
       final id = AppUtils.getIdFromHeader(header);
       final user = await managedContext.fetchObjectWithID<User>(id);
       user!.removePropertiesFromBackingMap(['refreshToken', 'accessToken']);
-      return AppResponse.ok(message: 'Успешное получения профиля', body: user.backing.contents);
+      return AppResponse.ok(
+          message: 'Успешное получения профиля', body: user.backing.contents);
     } catch (e) {
-      return AppResponse.serverError(e, message: 'Ошибка получения профиля пользователя');
+      return AppResponse.serverError(e,
+          message: 'Ошибка получения профиля пользователя');
+    }
+  }
+
+  @Operation.post()
+  Future<Response> updateProfile(
+      @Bind.header(HttpHeaders.authorizationHeader) String header,
+      @Bind.body() User user) async {
+    try {
+      final id = AppUtils.getIdFromHeader(header);
+      var found = await managedContext.fetchObjectWithID<User>(id);
+
+      // Запрос для обновления данных пользователя
+      final qUpdateUser = Query<User>(managedContext)
+        ..where((x) => x.id).equalTo(id)
+        ..values.userName = user.userName ?? found!.userName
+        ..values.email = user.email ?? found!.email;
+      await qUpdateUser.updateOne();
+
+      // Получаем обновленного пользователя
+      found = await managedContext.fetchObjectWithID<User>(id);
+      found!.removePropertiesFromBackingMap(['accessToken', 'refreshToken']);
+
+      return AppResponse.ok(
+          body: found.backing.contents, message: 'Успешное обновление профиля');
+    } catch (e) {
+      return AppResponse.serverError(e, message: 'Ошибка обновления данных');
     }
   }
 }
