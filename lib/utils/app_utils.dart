5ae758cf2ef3f94085a9c2e4544510d52ac5b894
WizMe-M <timkin.moxim@mail.ru>
2023-01-22 18:58:33 +0300

    id resolve method

--- lib/utils/app_utils.dart
+++ lib/utils/app_utils.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:conduit/conduit.dart';
 import 'package:jaguar_jwt/jaguar_jwt.dart';
 
 abstract class AppUtils {
@@ -9,5 +10,11 @@ abstract class AppUtils {
   static int getIdFromToken(String token) {
     final jwtClaim = verifyJwtHS256Signature(token, secretKey);
     return int.parse(jwtClaim['id'].toString());
+  }
+
+  static int getIdFromHeader(String header) {
+    final token = const AuthorizationBearerParser().parse(header);
+    final id = getIdFromToken(token ?? '');
+    return id;
   }
 }
